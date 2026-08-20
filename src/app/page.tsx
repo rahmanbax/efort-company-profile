fdc4916af27983fa2cbeb3e4023805641edd6d99
rahmanbax <basyirahman@gmail.com>
2026-08-21 01:21:44 +0700

    import fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
+import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div >
-      <main >
-        <div className="h-dvh text-center bg-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quaerat, aspernatur, et officia voluptatem ducimus nisi quisquam a deleniti voluptate, porro vel totam molestias aperiam. Recusandae doloremque nisi accusantium tempore.
-        </div>
-      </main>
-    </div>
-  );
+    <div>LandingPage</div>
+  )
 }
